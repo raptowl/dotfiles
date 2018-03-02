219bdeafd614b9e9072c79f2fad86909c123a1de
raptowl <k.fullcho.raptowl0110@gmail.com>
2018-03-03 02:05:48 +0900

    wrote undeploy

--- dotmgr.sh
+++ dotmgr.sh
@@ -1,33 +1,47 @@
 #!/bin/sh
 
 # define variables.
-DOT_PATH="$HOME/.dotfiles"
-DOT_EXCLUDE=". .. .git bin dotmgr.sh etc LICENSE README.md"
+dotpath="$HOME/.dotfiles"
+dotexclude='. .. .git LICENSE README.md bin dotmgr.sh etc lib'
 
 ############################################################
 
-if [ $# -eq 0 ]; then
-    :
-    exit 0
-elif [ $# -eq 1 ]; then
-    if [ $1 = "deploy" ]; then #deploy dotfiles.
-        for i in `ls -a $DOT_PATH`; do
-            for j in $DOT_EXCLUDE; do
-                if [ $i = $j ]; then
+if [ $# -eq 1 ]
+then
+    if [ $1 = 'deploy' ] # deploy dotfiles.
+    then
+        for i in `ls -a $dotpath`
+        do
+            for j in $dotexclude
+            do
+                if [ $i = $j ]
+                then
                     continue 2
                 fi
             done
-            ln -sf $DOT_PATH/$i $HOME/$i
-            echo $i
+            ln -sf $dotpath/$i $HOME/$i
         done
         exit 0
-    elif [ $1 = "undeploy" ]; then #undeploy dotfiles.
+    elif [ $1 = 'undeploy' ] # undeploy dotfiles.
+    then
+        for i in `ls -a $dotpath`
+        do
+            for j in $dotexclude
+            do
+                if [ $i = $j ]
+                then
+                    continue 2
+                fi
+            done
+            rm $HOME/$i
+        done
+        exit 0
+    elif [ $1 = 'install' ] # build software locally.
+    then
         :
         exit 0
-    elif [ $1 = "install" ]; then # build software locally
-        :
-        exit 0
-    elif [ $1 = "uninstall" ]; then # delete built software locally.
+    elif [ $1 = 'uninstall' ] # delete built software locally.
+    then
         :
         exit 0
     else # if any other instructions, return error.
