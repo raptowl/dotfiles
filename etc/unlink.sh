05aa1de1842ed4fcf356286895af22af841cb9a4
raptowl <katsumi.furusho@gmail.com>
2019-11-10 23:07:25 +0900

    fixed link.sh and unlink.sh

--- etc/unlink.sh
+++ etc/unlink.sh
@@ -9,16 +9,17 @@ path_src="$HOME/.dotfiles"
 
 # remove links at ~
 cd "$path_dst" || exit 1
-for line in * .*; do printf '%s\n' "$line"; done |
-  sed -e "s%^%$path_dst/%" |
-  xargs file |
-  awk -F ':' 'match($2, ".*symbolic link to.*'"$path_src"'") { print $1; }' |
-  sed -e "s%^.*\$%rm '&'%"
+for files in * .*; do printf '%s\n' "$files"; done |
+	sed -e "s%^%$path_dst/%" |
+	xargs file |
+	sed -e 's/:  */:/' |
+	awk -F ':' 'match($2, "symbolic link to.*'"$path_src"'") { print $1; }' |
+	sed -e "s%^.*\$%rm '&'%"
 
 # rename dotfiles which were original
-for line in * .*; do printf '%s\n' "$line"; done |
-  grep -e '\.dotold$' |
-  sed -e "s%^\(.*\)\.dotold\$%mv '$path_dst/\1.dotold' '$path_dst/\1'%"
+for files in * .*; do printf '%s\n' "$files"; done |
+	grep -e '\.dotold$' |
+	sed -e "s%^\(.*\)\.dotold\$%mv '$path_dst/\1.dotold' '$path_dst/\1'%"
 
 # about ~/.config
 path_dst="$HOME/.config"
@@ -26,13 +27,14 @@ path_src="$HOME/.dotfiles/.config"
 
 # remove links at ~/.config
 cd "$path_dst" || exit 1
-for line in * .*; do printf '%s\n' "$line"; done |
-  sed -e "s%^%$path_dst/%" |
-  xargs file |
-  awk -F ':' 'match($2, ".*symbolic link to.*'"$path_src"'") { print $1; }' |
-  sed -e "s%^.*\$%rm '&'%"
+for files in * .*; do printf '%s\n' "$files"; done |
+	sed -e "s%^%$path_dst/%" |
+	xargs file |
+	sed -e 's/:  */:/' |
+	awk -F ':' 'match($2, "symbolic link to.*'"$path_src"'") { print $1; }' |
+	sed -e "s%^.*\$%rm '&'%"
 
 # rename dotfiles which were original
-for line in * .*; do printf '%s\n' "$line"; done |
-  grep -e '\.dotold$' |
-  sed -e "s%^\(.*\)\.dotold\$%mv '$path_dst/\1.dotold' '$path_dst/\1'%"
+for files in * .*; do printf '%s\n' "$files"; done |
+	grep -e '\.dotold$' |
+	sed -e "s%^\(.*\)\.dotold\$%mv '$path_dst/\1.dotold' '$path_dst/\1'%"
