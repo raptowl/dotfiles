be92138efe9de2e694c472f5813d774fbcc7b183
raptowl <katsumi.furusho@gmail.com>
2019-11-17 23:08:51 +0900

    updated install scripts

---
+++ etc/installer/git-prompt.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -u
+umask 0022
+
+url='https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh'
+
+[ ! -d "$HOME/usr/local" ] && mkdir -p "$HOME/usr/local"
+
+cd "$HOME/usr/local" || exit 1
+if type curl > /dev/null 2>&1; then
+  curl -L "$url" > './git-prompt.sh'
+elif type wget > /dev/null 2>&1; then
+  wget -O - "$url" > './git-prompt.sh'
+else
+  printf 'ERROR: command curl or wget not found.\n' >&2
+fi
