bc78011026bac626d5b2bdbd1965238a2ce7d612
raptowl <katsumi.furusho@gmail.com>
2019-11-21 22:04:19 +0900

    updated lib/error_exit.sh and lib/warning.sh

--- lib/error_exit.sh
+++ lib/error_exit.sh
@@ -2,6 +2,6 @@
 
 # a function to output error message and exit with error status
 error_exit() {
-  printf '[ERROR] %s: %s\n' "${0##*/}" "$2" >&2
+  printf '\033[31m[ERROR]\033[0m %s: %s\n' "${0##*/}" "$2" >&2
   exit "$1"
 }
--- lib/warning.sh
+++ lib/warning.sh
@@ -2,5 +2,5 @@
 
 # a function to output warning message
 warning() {
-  printf '[WARNING] %s: %s\n' "${0##*/}" "$1" >&2
+  printf '\033[33m[WARNING]\033[0m %s: %s\n' "${0##*/}" "$1" >&2
 }
