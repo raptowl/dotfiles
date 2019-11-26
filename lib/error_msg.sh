a76b61c0948da9b2897ecaee5fdf6f6aa7dc4df1
raptowl <katsumi.furusho@gmail.com>
2019-11-26 22:57:48 +0900

    fixed function file names

--- lib/error_msg.sh
+++ lib/error_msg.sh
@@ -2,7 +2,7 @@
 
 ################################################################
 #
-# error.sh
+# error_msg.sh
 #   - output error message
 #
 # written by raptowl on 2019/11/26
@@ -10,6 +10,6 @@
 ################################################################
 
 # $1: error message to output to stderr
-error() {
-  printf '\033[31m[ERROR]\033[0m %s: %s\n' "${0##*/}" "$1" >&2
+error_msg() {
+  printf '\033[1;31m[ERROR]\033[0m %s: %s\n' "${0##*/}" "$1" >&2
 }
--- lib/warning_msg.sh
+++ lib/warning_msg.sh
@@ -2,7 +2,7 @@
 
 ################################################################
 #
-# warning.sh
+# warning_msg.sh
 #   - output warning message
 #
 # written by raptowl on 2019/11/22
@@ -10,6 +10,6 @@
 ################################################################
 
 # $1: message to output to stderr
-warning() {
-  printf '\033[33m[WARNING]\033[0m %s: %s\n' "${0##*/}" "$1" >&2
+warning_msg() {
+  printf '\033[1;33m[WARNING]\033[0m %s: %s\n' "${0##*/}" "$1" >&2
 }
