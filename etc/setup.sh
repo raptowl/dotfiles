1c6e02a304bdb8373e783a3915578fdec6b8f720
raptowl <katsumi.furusho@gmail.com>
2020-08-01 15:09:16 +0900

    fix bug on etc/setup.sh

--- etc/setup.sh
+++ etc/setup.sh
@@ -9,29 +9,29 @@ url_gitrepo='https://github.com/raptowl/dotfiles.git'
 url_tarball='github.com/raptowl/dotfiles/archive/master.tar.gz'
 
 # set a environment variable "$DOTFILES_DIR"
-DOTFILES_DIR=$(pwd | tr -d '\n')
+current_dir=$(pwd | tr -d '\n')
+DOTFILES_DIR="$current_dir/.dotfiles"
 export DOTFILES_DIR
 
 # make a dotfiles_dir file
 if mkdir -p "$HOME/usr/local"; then
   echo "$DOTFILES_DIR" > "$HOME/usr/local/dotfiles_dir"
 else
+  printf 'ERROR: could not make a directory at %s .\n' "$HOME/usr/local" >&2
   exit 1
 fi
 
 # main routine
-cd "$HOME" \
-  || exit 1
 if type git > /dev/null 2>&1; then
   git clone "$url_gitrepo" "$DOTFILES_DIR"
 elif type curl > /dev/null 2>&1; then
   curl -L "$url_tarball" \
     | tar xzv \
-    && mv -f "$HOME/dotfiles-master" "$DOTFILES_DIR"
+    && mv -f "$current_dir/dotfiles-master" "$DOTFILES_DIR"
 elif type wget > /dev/null 2>&1; then
   wget -O - "$url_tarball" \
     | tar xzv \
-    && mv -f "$HOME/dotfiles-master" "$DOTFILES_DIR"
+    && mv -f "$current_dir/dotfiles-master" "$DOTFILES_DIR"
 else
   printf 'ERROR: command git wget or curl not found.\n' >&2
   exit 1
