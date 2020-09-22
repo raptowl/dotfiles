7a7f8f925e0859287011c3d2deeab99b044f7854
raptowl <katsumi.furusho@gmail.com>
2020-09-22 15:00:39 +0900

    fix command and function template

--- etc/template/libfunc.sh
+++ etc/template/libfunc.sh
@@ -1,15 +1,13 @@
 #. "$DOTFILES_DIR/lib/logging_error.sh"
-################################
-# Short description here
-# Globals:
-#   None
-# Arguments:
-#   None
-# Outputs:
-#   None
-# Returns:
-#   None
-################################
 libfunc() {
+  # Short description here
+  # Globals:
+  #   None
+  # Arguments:
+  #   None
+  # Outputs:
+  #   None
+  # Returns:
+  #   None
   :
 }
