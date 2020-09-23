151218c44c2011d888e143d3358aba73c2c6995d
raptowl <katsumi.furusho@gmail.com>
2020-09-23 19:09:43 +0900

    integrate list_files_with_hidden

--- lib/check_dir_empty.sh
+++ lib/check_dir_empty.sh
@@ -1,18 +1,16 @@
-. "$DOTFILES_DIR/lib/list_files_with_hidden.sh"
-################################
-# Check the specified directory is empty
-# Globals:
-#   None
-# Arguments:
-#   target directory, string
-# Outputs:
-#   None
-# Returns:
-#   empty, 0
-#   not empty, 1
-#   directory not found, 2
-################################
+. "$DOTFILES_DIR/lib/list_files.sh"
 check_dir_empty() {
+  # Check the specified directory is empty
+  # Globals:
+  #   None
+  # Arguments:
+  #   target directory, string
+  # Outputs:
+  #   None
+  # Returns:
+  #   empty, 0
+  #   not empty, 1
+  #   directory not found, 2
   l_dir="${1:-.}"
   if [ ! -d "$l_dir" ]; then
     unset l_dir
@@ -20,7 +18,7 @@ check_dir_empty() {
   fi
 
   {
-    list_files_with_hidden "$l_dir"
+    list_files -h "$l_dir"
     unset l_dir
   } \
     | grep -e '^' > /dev/null 2>&1 \
--- lib/list_files.sh
+++ lib/list_files.sh
@@ -1,24 +1,44 @@
-################################
-# Print files list in the directory
-# Globals:
-#   None
-# Arguments:
-#   target directory, string
-# Outputs:
-#   filelist
-# Returns:
-#   success, 0
-#   directory not found, 1
-################################
 list_files() {
+  # Print files list in the directory
+  # Globals:
+  #   None
+  # Arguments:
+  #   [-h], list with hidden files
+  #   target directory, string
+  # Outputs:
+  #   filelist
+  # Returns:
+  #   success, 0
+  #   directory not found, 1
+  #   invalid options, 1
+  while :; do
+    case "$1" in
+      -h) lopt_hidden=true ;;
+      * ) break ;;
+    esac
+    shift
+  done
+  if [ $# -gt 1 ]; then
+    unset lopt_hidden
+    return 1
+  fi
+
   l_dir=$(printf '%s' "${1:-.}" \
             | sed -e 's%/$%%')
   if [ ! -d "$l_dir" ]; then
-    unset l_dir
+    unset lopt_hidden l_dir
     return 1
   fi
-  printf '%s\n' "$l_dir/"*
 
-  unset l_dir
+  if [ -n "${lopt_hidden:-}" ]; then
+    printf '%s\n' "$l_dir/"* "$l_dir/".* \
+      | grep -v -e '/\*$' \
+                -e '/\.$' \
+                -e '/\.\.$'
+  else
+    printf '%s\n' "$l_dir/"*
+  fi
+
+  unset lopt_hidden l_dir
   return 0
 }
